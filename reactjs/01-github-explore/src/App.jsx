f3c4544e1077c321befac1201f51be3b2225c191
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-18 09:18:34 -0300

    09 - Ambiente dev e produção

--- reactjs/01-github-explore/src/App.jsx
+++ reactjs/01-github-explore/src/App.jsx
@@ -1,5 +1,5 @@
-export function App(){
-    throw new Error('Eita Giovanna, o forninho caiu.')
+export function App() {
+    // throw new Error('Eita Giovanna, o forninho caiu.')
 
-    return <h1> Hello World!</h1>
+    return <h1>Hello Gabriel!</h1>
 }
--- reactjs/01-github-explore/webpack.config.js
+++ reactjs/01-github-explore/webpack.config.js
@@ -1,31 +1,39 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
+const isDevelopment = process.env.NODE_ENV !== "production"
+
+//https://webpack.js.org/configuration/
 module.exports = {
-    mode: 'development',
-    devtool: 'eval-source-map',
-    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    mode: isDevelopment ? "development" : "production", // "production" | "development" | "none"
+    devtool: isDevelopment ? "eval-source-map" : "source-map",
+    // Chosen mode tells webpack to use its built-in optimizations accordingly.
+    entry: path.resolve(__dirname, "src", "index.jsx"),
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
     },
     resolve: {
-        extensions: ['.js', '.jsx'],
+        extensions: [".js", ".jsx"],
     },
     devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
+        inline: true, // reload na pagina automaticamente
+        contentBase: path.resolve(__dirname, "public"),
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html'),
+            template: path.resolve(__dirname, "public", "index.html"),
         })
     ],
     module: {
+        // configuration regarding modules
         rules: [
+            // rules for modules (configure loaders, parser options, etc.)
             {
-                test: /\.jsx$/,
-                excluse: /node_modules/,
-                use: 'babel-loader'
+                // Conditions:
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
             }
         ]
     }

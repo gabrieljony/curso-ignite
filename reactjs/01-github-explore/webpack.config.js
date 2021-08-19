e9459bcf7381577f6b04f52c459ef221b6d75869
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-18 21:13:39 -0300

    11 - Utilizando SASS

--- reactjs/01-github-explore/webpack.config.js
+++ reactjs/01-github-explore/webpack.config.js
@@ -37,9 +37,9 @@ module.exports = {
             },
             {
                 // Conditions:
-                test: /\.css?$/,
+                test: /\.scss?$/,
                 exclude: /node_modules/,
-                use: ["style-loader", "css-loader"]
+                use: ["style-loader", "css-loader", "sass-loader"]
             },
         ]
     }

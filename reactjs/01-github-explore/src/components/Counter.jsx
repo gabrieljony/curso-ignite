b0150f663b95565a0afc2f3dbf281080fb02804a
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-18 21:55:57 -0300

    04 - A imutabilidade no React

--- reactjs/01-github-explore/src/components/Counter.jsx
+++ reactjs/01-github-explore/src/components/Counter.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { useState } from 'react'
 
 // Toda função do react que começar com use, Ex. useState, chamamos ela de hook ou gancho
-
+// Imutabilidade - prever que uma variavel que não vai ter seu valor alterado, mas sim da um novo valor para aquela váriavel
+//Ex.
+// usuarios = ['gabriel','pedro','lucas']
+// usuários.push('rafael') -> não é imutabilidade
+// novoUsuarios = [..usuarios, 'rafael']
 export function Counter(props) {
     // let -> let it change, que quer dizer deixe isso mudar
+    // setCounter -> setar um novo valor para a variavel counter
     let [counter, setCounter] = useState(0);
 
     function increment() {

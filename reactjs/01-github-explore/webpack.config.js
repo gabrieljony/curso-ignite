c830a1288305d8ce344cbb3f1c30037d97a56839
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-18 22:05:18 -0300

    05 - Fast Refresh no Webpack https://github.com/pmmmwh/react-refresh-webpack-plugin

--- reactjs/01-github-explore/webpack.config.js
+++ reactjs/01-github-explore/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== "production"
 
@@ -19,12 +20,14 @@ module.exports = {
     devServer: {
         inline: true, // reload na pagina automaticamente
         contentBase: path.resolve(__dirname, "public"),
+        hot: true
     },
     plugins: [
+        isDevelopment && new ReactRefreshWebpackPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "public", "index.html"),
         })
-    ],
+    ].filter(Boolean),
     module: {
         // configuration regarding modules
         rules: [
@@ -33,7 +36,14 @@ module.exports = {
                 // Conditions:
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                use: "babel-loader"
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        plugins: [
+                            isDevelopment && require.resolve('react-refresh/babel')
+                        ].filter(Boolean),
+                    }
+                }
             },
             {
                 // Conditions:

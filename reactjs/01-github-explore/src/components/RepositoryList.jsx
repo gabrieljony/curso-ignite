821071476b29680d2f6cc217e919a0a07cea9b27
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-19 18:16:20 -0300

    02 - Utilizando o useEffect

--- reactjs/01-github-explore/src/components/RepositoryList.jsx
+++ reactjs/01-github-explore/src/components/RepositoryList.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { RepositoryItem } from './RepositoryItem';
 import '../styles/repositories.scss'
 
+// Repositorio do GitHub https://api.github.com/users/gabrieljony/repos
+
+const repository = {
+    name: 'unform',
+    description: 'teste',
+    link: 'teste'
+}
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    // quando estiver com a lista vazia no segundo parametro no useEffect que é a dependencia,
+    // quer dizer que vai ser executado uma única vez assim que o component for criado
+    // ATENÇÃO: não deixar de usar o 2 parametro, pois fica em looping
+    useEffect(() => {
+        fetch('https://api.github.com/users/gabrieljony/repos')
+        .then(response => response.json())
+        .then(data => {
+            console.log('data', data)
+            setRepositories(data)
+        })
+
+    }, []);
+
+    console.log('repositories', repositories)
+
     return (
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
             <ul>
                 <RepositoryItem repository="unform2"/>
-                <RepositoryItem repository="unform3"/>
-                <RepositoryItem repository="unform4"/>
-                <RepositoryItem repository="unform5"/>
             </ul>
         </section>
     )

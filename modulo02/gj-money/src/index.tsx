52d8f488e033699a281c434257f3b83c0a45f028
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-30 07:25:49 -0300

    02 - Configurando MirageJS

--- modulo02/gj-money/src/index.tsx
+++ modulo02/gj-money/src/index.tsx
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'v1';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: "transaction 1",
+          amount: 400,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date()
+        },
+        {
+          id: 2,
+          title: "transaction 2",
+          amount: 500,
+          type: "deposit",
+          category: "Food",
+          createdAt: new Date()
+        },
+        {
+          id: 3,
+          title: "transaction 3",
+          amount: 200,
+          type: "withdraw",
+          category: "Food",
+          createdAt: new Date()
+        },
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>

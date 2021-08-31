980659eb8c67b8ecab6f04d343856ac0dca6a4f5
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-08-31 20:08:13 -0300

    02 - A Context API no React

--- modulo02/gj-money/src/App.tsx
+++ modulo02/gj-money/src/App.tsx
@@ -4,6 +4,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
+import { TransactionsContext } from './TransactionsContext';
 
 // Questão de acessibilidade
 // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
@@ -21,7 +22,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal 
@@ -29,6 +30,6 @@ export function App() {
         onCloseNewTransactionModal={handleCloseNewTransactionModal}
         />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
--- modulo02/gj-money/src/components/Summary/index.tsx
+++ modulo02/gj-money/src/components/Summary/index.tsx
@@ -1,9 +1,14 @@
-import entradasImg from '../../assets/entradas.svg'
-import saidasImg from '../../assets/saidas.svg'
-import totalImg from '../../assets/total.svg'
-import { Container } from './styles'
+import { useContext } from 'react';
+import entradasImg from '../../assets/entradas.svg';
+import saidasImg from '../../assets/saidas.svg';
+import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
+import { Container } from './styles';
 
 export function Summary() {
+    const data = useContext(TransactionsContext);
+    console.log('data', data)
+
     return (
         <Container>
             <div>

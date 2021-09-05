9609f6c03c8b8cb3feda9deed7cbc9354db2f313
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-09-05 16:08:25 -0300

    01 - Componente Header

--- modulo03/ignews/src/pages/_app.tsx
+++ modulo03/ignews/src/pages/_app.tsx
@@ -1,9 +1,16 @@
+// Figma da aplicação: https://www.figma.com/file/gl0fHkQgvaUfXNjuwGtDDs/ig.news?node-id=1%3A2
 import { AppProps } from 'next/app';
+import Header from '../components/Header';
 
 import '../styles/global.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

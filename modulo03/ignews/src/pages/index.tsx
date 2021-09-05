511164c3627359302a657fdc5fd6146199cdf6b7
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-09-05 18:09:25 -0300

    03 - Página Home

--- modulo03/ignews/src/pages/index.tsx
+++ modulo03/ignews/src/pages/index.tsx
@@ -1,12 +1,31 @@
-import Head from "next/head"
+import Head from "next/head";
+import Image from 'next/image';
+import welcome from '../../public/images/welcome.svg';
+import mulher from '../../public/images/mulher.svg';
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
     <>
-      <Head><title>Início | ig.news</title></Head>
-      <h1>
-        Hello Next.js
-      </h1>
+      <Head><title>Home | ig.news</title></Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <Image 
+                  src={welcome} 
+                  alt="welcome"/>
+          <span>Hey, Welcome</span>
+          <h1>News about <br /> the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+
+        </section>
+
+        <Image 
+                src={mulher} 
+                alt="mulher no computador"/>
+      </main>
     </>
   )
 }

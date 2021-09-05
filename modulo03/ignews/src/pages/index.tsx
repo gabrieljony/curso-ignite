14d914594047156797dbd0aa245d7903b0fac726
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-09-05 19:17:32 -0300

    02 - Consumindo API do Stripe (SSR)

--- modulo03/ignews/src/pages/index.tsx
+++ modulo03/ignews/src/pages/index.tsx
@@ -1,11 +1,21 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from 'next/image';
 import welcome from '../../public/images/welcome.svg';
 import mulher from '../../public/images/mulher.svg';
 import styles from './home.module.scss'
 import SubscribeButton from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  console.log(product)
   return (
     <>
       <Head><title>Home | ig.news</title></Head>
@@ -18,10 +28,10 @@ export default function Home() {
           <h1>News about <br /> the <span>React</span> world.</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId}/>
 
         </section>
 
@@ -31,4 +41,28 @@ export default function Home() {
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  
+  // é executado no servidor node.js
+  console.log('ok - será mostrado no console.log do terminal não do navegador.')
+
+  const price = await stripe.prices.retrieve('price_1JWT0ZJ2mSTNh7jg7sf8SA7V', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-Us', {
+      style: 'currency',
+      currency: 'USD'
+  }).format(price.unit_amount / 100)
+};
+
+  return {
+      props:{
+        product
+      }
+  }
 }

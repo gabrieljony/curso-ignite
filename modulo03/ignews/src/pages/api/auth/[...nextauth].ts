731ccfcb6981564e6dee7a9a7da603dbc06641dc
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-09-07 09:50:06 -0300

    04 - Salvando usuário no banco

--- modulo03/ignews/src/pages/api/auth/[...nextauth].ts
+++ modulo03/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
 // instalação: npm i next-auth
 // https://next-auth.js.org/getting-started/example
 
-import NextAuth from "next-auth"
-import Provider from "next-auth/providers"
+import NextAuth from "next-auth";
+import Provider from "next-auth/providers";
+
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,6 +17,30 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY
+  },
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      console.log('user', user)
+
+      // Inserção no bando de dados NoSQL FaunaDB
+      // https://docs.fauna.com/fauna/current/api/fql/functions/create?lang=shell
+      // try{
+      //   await fauna.query(
+      //     q.Create(
+      //       q.Collection('users'),
+      //       { data: {email} }
+      //     )
+      //   )
+      //   return true 
+      // } catch {
+      //   return false
+      // }
+      return true
+      
+    },
+  }
 })
 
 // FaunaDB - HTTP

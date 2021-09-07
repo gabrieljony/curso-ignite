e562cd581c996326ec50f761ac8f8d8b01adb4e4
gabriel.borges.silva <gabriel.borges.silva@avanade.com>
2021-09-06 22:01:26 -0300

    01 - API routes no Next.js

---
+++ modulo03/ignews/src/pages/api/users.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+//API rotes do next - http://localhost:3000/api/users
+//Executada com o conceito de Serverless, só sobe e desce de acordo que é chamado a requisição
+export default (request: NextApiRequest, response: NextApiResponse) => {
+    const users = [
+        {id: 1, name: 'Diego'},
+        {id: 2, name: 'Ana'},
+        {id: 3, name: 'Rafael'},
+        {id: 4, name: 'Lucas'},
+    ]
+
+    return response.json(users)
+}
